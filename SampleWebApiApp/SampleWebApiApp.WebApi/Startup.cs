d1fd793e1fe5c2399dea4b62cc4db7b477ac3fbc
sibille <sibille@ilitia.com>
2020-05-26 19:03:13 +0200

    Add comment to Configure method

--- SampleWebApiApp/SampleWebApiApp.WebApi/Startup.cs
+++ SampleWebApiApp/SampleWebApiApp.WebApi/Startup.cs
@@ -35,6 +35,7 @@ namespace SampleWebApiApp.WebApi
             });
         }
 
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
